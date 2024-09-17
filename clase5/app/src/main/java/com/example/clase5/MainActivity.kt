8c6285e3b90c62f4b1ad134ca10edbd988056709
ulises-muller <ulisesmuller667@gmail.com>
2024-09-16 21:04:18 -0300

    as

--- clase5/app/src/main/java/com/example/clase5/MainActivity.kt
+++ clase5/app/src/main/java/com/example/clase5/MainActivity.kt
@@ -12,12 +12,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var spinner: Spinner
     private lateinit var listView: ListView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         spinner = findViewById(R.id.spinnerPais)
         listView = findViewById(R.id.listEquipos)
+
+        var equipo = mutableListOf("no cargo")
 
         val pais = listOf("Uruguay","Argentina","Colombia")
 
@@ -26,23 +29,31 @@ class MainActivity : AppCompatActivity() {
         val equipoCol = listOf("América de Cali","Millonarios","Atlético Nacional")
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, pais)
-        val adapterArg = ArrayAdapter(this, android.R.layout.simple_list_item_1, equipoArg)
-        val adapterUru = ArrayAdapter(this, android.R.layout.simple_list_item_1, equipoUru)
-        val adapterCol = ArrayAdapter(this, android.R.layout.simple_list_item_1, equipoCol)
+        val adapterList = ArrayAdapter(this, android.R.layout.simple_list_item_1, equipo)
 
         spinner.adapter = adapter
-
+        listView.adapter = adapterList
 
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val paises = spinner.selectedItem.toString()
-                if (paises == "Argentina") {
-                    listView.adapter = adapterArg
-                } else if (paises == "Uruguay") {
-                    listView.adapter = adapterUru
-                } else if (paises == "Colombia") {
-                    listView.adapter = adapterCol
+                val paises = pais[position]
+                when (paises) {
+                    "Argentina" -> {
+                        equipo.clear()
+                        equipo.addAll(equipoArg)
+                        adapterList.notifyDataSetChanged()
+                    }
+                    "Uruguay" -> {
+                        equipo.clear()
+                        equipo.addAll(equipoUru)
+                        adapterList.notifyDataSetChanged()
+                    }
+                    "Colombia" -> {
+                        equipo.clear()
+                        equipo.addAll(equipoCol)
+                        adapterList.notifyDataSetChanged()
+                    }
                 }
             }
 
